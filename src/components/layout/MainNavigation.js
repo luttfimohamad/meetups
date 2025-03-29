a39bcbfd78865485943b433bec176b4b0e80fae0
luttfimohamad <luttfimohamad@gmail.com>
2025-03-29 13:39:56 +0700

    Enhance MainNavigation component with styling and layout adjustments

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
 
 export default function MainNavigation() {
   return (
-    <header>
-      <div>Meetups App</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>Meetups App</div>
       <nav>
         <ul>
           <li>
