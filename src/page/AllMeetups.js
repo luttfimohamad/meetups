9a1bc3faa52eeb33d236a52dd7d9a51adf909d83
luttfimohamad <luttfimohamad@gmail.com>
2025-03-29 13:49:35 +0700

    Add dummy data and render meetup list in AllMeetups component

--- src/page/AllMeetups.js
+++ src/page/AllMeetups.js
@@ -1,5 +1,42 @@
 import React from 'react';
 
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
+
 export default function AllMeetups() {
-  return <div>AllMeetups</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return (
+            <li key={meetup.id}>
+              <h2>{meetup.title}</h2>
+              <img src={meetup.image} alt={meetup.title} />
+              <address>{meetup.address}</address>
+              <p>{meetup.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
