fb1aaeace44689fc759cdd7b99b8b0a03aaa7932
luttfimohamad <luttfimohamad@gmail.com>
2025-05-01 18:37:12 +0700

    Pass onAddMeetup prop to NewMeetupForm for handling new meetup submissions

--- src/page/NewMeetups.js
+++ src/page/NewMeetups.js
@@ -2,10 +2,22 @@ import React from 'react';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 export default function NewMeetups() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://api-meetups-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
