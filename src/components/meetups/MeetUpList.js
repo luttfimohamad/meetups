34e79c9cc275dd472c1b1445d01d2c2dc23e1803
luttfimohamad <luttfimohamad@gmail.com>
2025-05-04 20:40:45 +0700

    Refactor MeetUpList to handle empty meetups array and update AllMeetups to fetch data from API instead of using dummy data

--- src/components/meetups/MeetUpList.js
+++ src/components/meetups/MeetUpList.js
@@ -5,16 +5,20 @@ import MeetUpItem from './MeetUpItem';
 export default function MeetUpList(props) {
   return (
     <ul className={classes.list}>
-      {props.meetups.map((meetup) => (
-        <MeetUpItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-          description={meetup.description}
-        />
-      ))}
+      {Array.isArray(props.meetups) && props.meetups.length > 0 ? (
+        props.meetups.map((meetup) => (
+          <MeetUpItem
+            key={meetup.id}
+            id={meetup.id}
+            image={meetup.image}
+            title={meetup.title}
+            address={meetup.address}
+            description={meetup.description}
+          />
+        ))
+      ) : (
+        <p>No meetups found.</p>
+      )}
     </ul>
   );
 }
--- src/page/AllMeetups.js
+++ src/page/AllMeetups.js
@@ -1,33 +1,48 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import MeetUpList from '../components/meetups/MeetUpList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
-
 export default function AllMeetups() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://api-meetups-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
   return (
     <section>
       <ul>
         <h1>All Meetups</h1>
-        <MeetUpList meetups={DUMMY_DATA} />
+        <MeetUpList meetups={loadedMeetups} />
       </ul>
     </section>
   );
